a2de613956b1a8cef87ca4cb5d67be9c80436034
Yugratna <84085542+yugratna19@users.noreply.github.com>
2022-10-18 20:55:47 +0545

    Add files via upload

--- FormulaManager/mainwindow.cpp
+++ FormulaManager/mainwindow.cpp
@@ -19,39 +19,17 @@ void MainWindow::on_pushButton_clicked()
     QPushButton* target = qobject_cast<QPushButton*>(sender());
     if (target != nullptr)
     {
-        target->setStyleSheet(QString("#%1 { background-color: red; }").arg(target->objectName()));
+        target->setStyleSheet(QString("#%1 { background-color: green; }").arg(target->objectName()));
     }
+
     QSqlDatabase db = QSqlDatabase:: addDatabase("QMYSQL");
     db.setHostName("localhost");
     db.setUserName("root");
     db.setPassword("password");
     db.setDatabaseName("login");
-
-    if (db.open()){
-        QMessageBox::information(this,"Connection","Database Connected.");
-    }
-    else {
-        QMessageBox::information(this,"Not Connection","Database Not Connected.");
-    }
 }
 
-/*#include "mainwindow.h"
-#include "ui_mainwindow.h"
-
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-    connect(ui->myPushButton, &QPushButton::clicked, this, &MainWindow::onClicked);
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
-void MainWindow::onClicked()
+void MainWindow::on_pushButton_2_clicked()
 {
     QPushButton* target = qobject_cast<QPushButton*>(sender());
     if (target != nullptr)
@@ -59,4 +37,4 @@ void MainWindow::onClicked()
         target->setStyleSheet(QString("#%1 { background-color: red; }").arg(target->objectName()));
     }
 }
-*/
+
